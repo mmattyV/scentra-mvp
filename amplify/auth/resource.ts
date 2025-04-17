8596dc16f7d56f2f7dbd9193442e87c4fa831e76
Matthew Vu <mvu@college.harvard.edu>
2025-04-17 12:38:19 -0400

    readd auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,33 +5,33 @@ import { getUserAttributes } from "../data/get-user-attributes/resource";
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-// export const auth = defineAuth({
-//   loginWith: {
-//     email: true,
-//   },
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
 
-//   userAttributes: {
-//     // Standard attributes
-//     givenName: {
-//       required: true,
-//       mutable: true,
-//     },
-//     familyName: {
-//       required: true,
-//       mutable: true,
-//     },
-//     // Maps to Cognito standard attribute 'address'
-//     address: {
-//       mutable: true,
-//       required: false,
-//     },
-//   },
+  userAttributes: {
+    // Standard attributes
+    givenName: {
+      required: true,
+      mutable: true,
+    },
+    familyName: {
+      required: true,
+      mutable: true,
+    },
+    // Maps to Cognito standard attribute 'address'
+    address: {
+      mutable: true,
+      required: false,
+    },
+  },
   
-//   // Define admin group for user management
-//   groups: ["ADMINS"],
+  // Define admin group for user management
+  groups: ["ADMINS"],
   
-//   // Grant specific permissions to our function to access user data
-//   access: (allow) => [
-//     allow.resource(getUserAttributes).to(["getUser", "listUsers"])
-//   ]
-// });
+  // Grant specific permissions to our function to access user data
+  access: (allow) => [
+    allow.resource(getUserAttributes).to(["getUser", "listUsers"])
+  ]
+});
